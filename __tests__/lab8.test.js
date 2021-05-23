32bef10f7a62ff91797fe3e6cc048ad43493b98c
j2c0 <59665100+j2c0@users.noreply.github.com>
2021-05-22 19:52:05 -0700

    Add files via upload

--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -1,43 +1,54 @@
-describe('Basic user flow for SPA ', () => {
+describe("Basic user flow for SPA ", () => {
   beforeAll(async () => {
-    await page.goto('http://127.0.0.1:5500');
+    await page.goto("http://127.0.0.1:5500");
     await page.waitForTimeout(500);
   });
 
   // test 1 is given
-  it('Test1: Initial Home Page - Check for 10 Journal Entries', async () => {
-    const numEntries = await page.$$eval('journal-entry', (entries) => {
+  it("Test1: Initial Home Page - Check for 10 Journal Entries", async () => {
+    const numEntries = await page.$$eval("journal-entry", (entries) => {
       return entries.length;
     });
     expect(numEntries).toBe(10);
   });
 
   // test 2 is given
-  it('Test2: Make sure <journal-entry> elements are populated', async () => {
+  it("Test2: Make sure <journal-entry> elements are populated", async () => {
     let allArePopulated = true;
     let data, plainValue;
-    const entries = await page.$$('journal-entry');
+    const entries = await page.$$("journal-entry");
     for (let i = 0; i < entries.length; i++) {
-      data = await entries[i].getProperty('entry');
+      data = await entries[i].getProperty("entry");
       plainValue = await data.jsonValue();
-      if (plainValue.title.length == 0) { allArePopulated = false; }
-      if (plainValue.date.length == 0) { allArePopulated = false; }
-      if (plainValue.content.length == 0) { allArePopulated = false; }
+      if (plainValue.title.length == 0) {
+        allArePopulated = false;
+      }
+      if (plainValue.date.length == 0) {
+        allArePopulated = false;
+      }
+      if (plainValue.content.length == 0) {
+        allArePopulated = false;
+      }
     }
     expect(allArePopulated).toBe(true);
   }, 30000);
 
-  it('Test3: Clicking first <journal-entry>, new URL should contain /#entry1', async () => {
+  it("Test3: Clicking first <journal-entry>, new URL should contain /#entry1", async () => {
     // implement test3: Clicking on the first journal entry should update the URL to contain “/#entry1”
-
+    await page.click("journal-entry");
+    const url = await page.url();
+    expect(url).toMatch("http://127.0.0.1:5500/#entry1");
   });
 
-  it('Test4: On first Entry page - checking page header title', async () => {
-    // implement test4: Clicking on the first journal entry should update the header text to “Entry 1” 
-
+  it("Test4: On first Entry page - checking page header title", async () => {
+    // implement test4: Clicking on the first journal entry should update the header text to “Entry 1”
+    const header = await page.$eval("h1", (entry) => {
+      return entry.innerHTML;
+    });
+    expect(header).toBe("Entry 1");
   });
 
-  it('Test5: On first Entry page - checking <entry-page> contents', async () => {
+  it("Test5: On first Entry page - checking <entry-page> contents", async () => {
     /*
      implement test5: Clicking on the first journal entry should contain the following contents: 
         { 
@@ -50,51 +61,69 @@ describe('Basic user flow for SPA ', () => {
           }
         }
       */
-
+    const entry = await page.$("entry-page");
+    const data = await entry.getProperty("entry");
+    const jsonEntry = await data.jsonValue();
+    expect(jsonEntry.title).toMatch("You like jazz?");
+    expect(jsonEntry.date).toMatch("4/25/2021");
+    expect(jsonEntry.content).toMatch(
+      "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible."
+    );
+    expect(jsonEntry.image.alt).toMatch("bee with sunglasses");
+    expect(jsonEntry.image.src).toMatch(
+      "https://i1.wp.com/www.thepopcornmuncher.com/wp-content/uploads/2016/11/bee-movie.jpg?resize=800%2C455"
+    );
   }, 10000);
 
-  it('Test6: On first Entry page - checking <body> element classes', async () => {
+  it("Test6: On first Entry page - checking <body> element classes", async () => {
     // implement test6: Clicking on the first journal entry should update the class attribute of <body> to ‘single-entry’
-
+    const journalEntry = await page.$eval("body", (entry) => {
+      return entry.classList[0];
+    });
+    expect(journalEntry).toEqual("single-entry");
   });
 
-  it('Test7: Clicking the settings icon, new URL should contain #settings', async () => {
+  it("Test7: Clicking the settings icon, new URL should contain #settings", async () => {
     // implement test7: Clicking on the settings icon should update the URL to contain “/#settings”
-
+    await page.click("img");
+    const url = await page.url();
+    expect(url).toBe("http://127.0.0.1:5500/#settings");
   });
 
-  it('Test8: On Settings page - checking page header title', async () => {
+  it("Test8: On Settings page - checking page header title", async () => {
     // implement test8: Clicking on the settings icon should update the header to be “Settings”
-
+    const header = await page.$eval("h1", (settingHeader) => {
+      return settingHeader.innerHTML;
+    });
+    expect(header).toMatch("Settings");
   });
 
-  it('Test9: On Settings page - checking <body> element classes', async () => {
+  it("Test9: On Settings page - checking <body> element classes", async () => {
     // implement test9: Clicking on the settings icon should update the class attribute of <body> to ‘settings’
-
+    const settingsBodyClass = await page.$eval("body", (settings) => {
+      return settings.classList;
+    });
+    expect(settingsBodyClass[0]).toBe("settings");
   });
 
-  it('Test10: Clicking the back button, new URL should be /#entry1', async() => {
+  it("Test10: Clicking the back button, new URL should be /#entry1", async () => {
     // implement test10: Clicking on the back button should update the URL to contain ‘/#entry1’
-
+    await page.goBack();
+    const url = await page.url();
+    expect(url).toBe("http://127.0.0.1:5500/#entry1");
   });
 
   // define and implement test11: Clicking the back button once should bring the user back to the home page
 
-
   // define and implement test12: When the user if on the homepage, the header title should be “Journal Entries”
 
-
-  // define and implement test13: On the home page the <body> element should not have any class attribute 
-
+  // define and implement test13: On the home page the <body> element should not have any class attribute
 
   // define and implement test14: Verify the url is correct when clicking on the second entry
 
-
   // define and implement test15: Verify the title is current when clicking on the second entry
 
-
   // define and implement test16: Verify the entry page contents is correct when clicking on the second entry
-
 
   // create your own test 17
 
@@ -103,5 +132,4 @@ describe('Basic user flow for SPA ', () => {
   // create your own test 19
 
   // create your own test 20
-  
 });
