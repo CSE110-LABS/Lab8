addc7163d58b1e2fcd68d2322407348e35916e1e
Hana Kim <hsk003@ucsd.edu>
2021-05-23 03:37:17 -0700

    added final 10 tests

--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -114,22 +114,96 @@ describe("Basic user flow for SPA ", () => {
   });
 
   // define and implement test11: Clicking the back button once should bring the user back to the home page
+  it("Test11: Clicking the back button, back to home page", async () => {
+    await page.goBack();
+    const url = await page.url();
+    expect(url).toBe("http://127.0.0.1:5500/");
+  });
 
   // define and implement test12: When the user if on the homepage, the header title should be “Journal Entries”
+  it("Test12: When user on home page, header title should be “Journal Entries", async () => {
+    const header = await page.$eval("h1", (entry) => {
+      return entry.innerHTML;
+    });
+    expect(header).toBe("Journal Entries");
+  });
 
   // define and implement test13: On the home page the <body> element should not have any class attribute
+  it("Test12: When user on home page, header title should be “Journal Entries", async () => {
+    const classLen = await page.$eval("body", (entry) => {
+      return entry.classList.length;
+    });
+    expect(classLen).toBe(0);
+  });
 
   // define and implement test14: Verify the url is correct when clicking on the second entry
+  it('Test14: When click on second entry, url is correct', async() => {
+    const entries = await page.$$("journal-entry");
+    await entries[1].click();
+    const url = await page.url();
+    expect(url).toBe("http://127.0.0.1:5500/#entry2");
+  });
 
   // define and implement test15: Verify the title is current when clicking on the second entry
+  it('Test15: When click on second entry, title is correct', async() => {
+    const header = await page.$eval("header > h1", (entry) => {
+      return entry.innerHTML;
+    });
+    expect(header).toBe("Entry 2");
+  });
 
   // define and implement test16: Verify the entry page contents is correct when clicking on the second entry
+  it('Test16: When clicking on the second entry, entry page contents is correct', async() => {
+    const entries = await page.$$("journal-entry");
+    const entry = entries[1];
+    const data = await entry.getProperty("entry");
+    const jsonEntry = await data.jsonValue();
+
+    expect(jsonEntry.title).toMatch("Run, Forrest! Run!");
+    expect(jsonEntry.date).toMatch("4/26/2021"); 
+    expect(jsonEntry.content).toMatch("Mama always said life was like a box of chocolates. You never know what you're gonna get.");
+    expect(jsonEntry.image.src).toMatch("https://s.abcnews.com/images/Entertainment/HT_forrest_gump_ml_140219_4x3_992.jpg");
+    expect(jsonEntry.image.alt).toMatch("forrest running");
+    expect(data.audio).toBeUndefined();
+  }, 10000);
 
   // create your own test 17
+  it("Test17: Clicking the back button from second entry, back to home page", async () => {
+    await page.goBack();
+    const url = await page.url();
+    expect(url).toBe("http://127.0.0.1:5500/");
+  });
 
   // create your own test 18
+  it('Test18: When click on third entry, url is correct', async() => {
+    const entries = await page.$$("journal-entry");
+    await entries[2].click();
+    await page.waitForNavigation();
+    const url = await page.url();
+    expect(url).toBe("http://127.0.0.1:5500/#entry3");
+  }, 30000);
 
   // create your own test 19
+  it('Test19: When click on third entry, title is correct', async() => {
+    const header = await page.$eval("header > h1", (entry) => {
+      return entry.innerHTML;
+    });
+    expect(header).toBe("Entry 3");
+  });
 
   // create your own test 20
+  it('Test20: When clicking on the third entry, entry page contents is correct', async() => {
+    const entries = await page.$$("journal-entry");
+    const entry = entries[2];
+    await page.waitForNavigation();
+    const data = await entry.getProperty("entry");
+    const jsonEntry = await data.jsonValue();
+
+    expect(jsonEntry.title).toMatch("Ogres are like onions");
+    expect(jsonEntry.date).toMatch("4/27/2021"); 
+    expect(jsonEntry.content).toMatch("Onions have layers. Ogres have layers. Onions have layers. You get it? We both have layers.");
+    expect(jsonEntry.image.src).toMatch("https://advancelocal-adapter-image-uploads.s3.amazonaws.com/image.syracuse.com/home/syr-media/width2048/img/entertainment_impact/photo/shrek-donkeyjpg-daa31aa2b5bedfaa.jpg");
+    expect(jsonEntry.image.alt).toMatch("shrek and donkey looking confused");
+    expect(data.audio).toBeUndefined();
+  }, 30000);
 });
