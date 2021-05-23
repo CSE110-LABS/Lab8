e9d7d43ce848a5a605093bbb32e780378c83d93a
j2c0 <59665100+j2c0@users.noreply.github.com>
2021-05-22 17:34:45 -0700

    Add files via upload

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -2,3 +2,25 @@
  * @jest-environment jsdom
  */
 
+import { pushToHistory } from "../scripts/router.js";
+
+describe("pushToHistory test", () => {
+  let currHistory;
+  test("test settings state", () => {
+    currHistory = pushToHistory("settings");
+    expect(currHistory.state.page).toBe("settings");
+    expect(currHistory.length).toBe(history.length);
+  });
+
+  test("test entry state", () => {
+    currHistory = pushToHistory("entry", 1);
+    expect(currHistory.state.page).toBe("entry1");
+    expect(currHistory.length).toBe(history.length);
+  });
+
+  test("test default state", () => {
+    const currHistory = pushToHistory();
+    expect(currHistory.state.page).toBe(undefined);
+    expect(currHistory.length).toBe(history.length);
+  });
+});
